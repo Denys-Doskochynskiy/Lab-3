7b33a77197fc73dd9435759ffc4db7e5661efe79
Denys-Doskochynskiy <55959253+Denys-Doskochynskiy@users.noreply.github.com>
2020-03-01 14:45:24 +0200

    Change method
    I change method findGoodByPriceInUAH to findGoodByGenreType

--- java/ua/lviv/iot/manager/BookstoreManager.java
+++ java/ua/lviv/iot/manager/BookstoreManager.java
@@ -3,6 +3,7 @@ package ua.lviv.iot.manager;
 import java.util.LinkedList;
 import java.util.List;
 import ua.lviv.iot.model.AbstractBookstore;
+import ua.lviv.iot.model.GenreType;
 
 public class BookstoreManager {
 
@@ -12,13 +13,13 @@ public class BookstoreManager {
 		this.goods = goods;
 	}
 
-	public List<AbstractBookstore> findGoodsCheaperThan(double price, List<AbstractBookstore> goods) {
+	public List<AbstractBookstore> findGoodsCheaperThan(GenreType genreType, List<AbstractBookstore> goods) {
 		List<AbstractBookstore> result = new LinkedList<>();
 		for (int goodFromTheList = 0; goodFromTheList < goods.size(); goodFromTheList++) {
-			if (goods.get(goodFromTheList).getPriceInUAH() < price) {
+			if (goods.get(goodFromTheList).getGenreType() == genreType) {
 				result.add(goods.get(goodFromTheList));
 				System.out.println("#" + (goodFromTheList + 1) + " matching the criteria" + " with price "
-						+ goods.get(goodFromTheList).getPriceInUAH());
+						+ goods.get(goodFromTheList).getGenreType());
 			} else {
 				System.out.println(" #" + (goodFromTheList + 1) + " does not match the criteria");
 			}
