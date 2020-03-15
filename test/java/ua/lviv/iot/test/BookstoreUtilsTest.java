86f1db2e550bdaf5e35159611b6498457e0ddc92
Denys-Doskochynskiy <55959253+Denys-Doskochynskiy@users.noreply.github.com>
2020-03-15 22:43:30 +0200

    Fix some bug and add test from lab5;

--- test/java/ua/lviv/iot/test/BookstoreUtilsTest.java
+++ test/java/ua/lviv/iot/test/BookstoreUtilsTest.java
@@ -7,21 +7,32 @@ import org.junit.jupiter.api.Test;
 
 class BookstoreUtilsTest extends GeneralBookstoreManagerTest {
 
+	
 	@Test
-	public void testSortGoodsByPrice_ASCENDING() {
-		BookstoreManagerUtils.sortGoodsByPriceInUAH_ASCENDING(goods, SortType.ASCENDING);
-		assertEquals(1550.65, goods.get(0).getPriceInUAH());
-		assertEquals(150.55, goods.get(1).getPriceInUAH());
-		assertEquals(28.69, goods.get(2).getPriceInUAH());
-		assertEquals(258.05, goods.get(3).getPriceInUAH());
+	public void testSortGoodsByName() {
+		BookstoreManagerUtils.sortGoodsByName(goods, SortType.ASCENDING);
+		assertEquals("AllIn1", goods.get(0).getName());
+		assertEquals("BestRes", goods.get(1).getName());
+		assertEquals("Math200", goods.get(2).getName());
+		assertEquals("Test", goods.get(3).getName());
 	}
 
 	@Test
-	public void testSortGoodsByPrice_DESCENDING() {
-		BookstoreManagerUtils.sortGoodsByPriceInUAH_DESCENDING(goods, SortType.DESCENDING);
-		assertEquals(1550.65, goods.get(0).getPriceInUAH());
+
+	public void testSortGoodsByPriceInUAH() {
+		BookstoreManagerUtils.sortByPriceInUAH(goods, SortType.ASCENDING);
+		assertEquals(28.69, goods.get(0).getPriceInUAH());
 		assertEquals(150.55, goods.get(1).getPriceInUAH());
-		assertEquals(28.69, goods.get(2).getPriceInUAH());
-		assertEquals(258.05, goods.get(3).getPriceInUAH());
+		assertEquals(258.05, goods.get(2).getPriceInUAH());
+		assertEquals(1550.65, goods.get(3).getPriceInUAH());
+	}
+
+	@Test
+	public void testSortGoodsByNuberOfPages() {
+		BookstoreManagerUtils.SortByNumberOfPages(goods, SortType.ASCENDING);
+		assertEquals(140, goods.get(0).getNumberOfPages());
+		assertEquals(256, goods.get(1).getNumberOfPages());
+		assertEquals(295, goods.get(2).getNumberOfPages());
+		assertEquals(1690, goods.get(3).getNumberOfPages());
 	}
 }
